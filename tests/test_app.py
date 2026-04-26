25656c362be21c4e8738670c781ee44aa99c5a84
ranjit-walunj <ranjitwalunj4@gmail.com>
2026-04-26 14:48:38 +0530

    changed the apptest file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,19 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_hello(client):
     """Test the home route."""
     rv = client.get('/')
     assert rv.status_code == 200
     assert rv.get_json() == {"message": "Hello, CI/CD!"}
+
 
 def test_health(client):
     """Test the health check route."""
